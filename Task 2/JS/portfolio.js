c00a65f7dc8a6b4daa18de49fbb70884c161bc60
selamalex <121111132+selamalex@users.noreply.github.com>
2023-09-14 23:09:21 +0300

    final portfolio

---
+++ Task 2/JS/portfolio.js
@@ -0,0 +1,7 @@
+let slider = new Typed(".slider", {
+    strings: ["Computer Science Student", "Front-End web developer", "UI/UX Designer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
